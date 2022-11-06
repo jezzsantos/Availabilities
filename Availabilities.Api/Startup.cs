5721cc347d5ba07d417ce31c47cd115cae48911b
Jezz Santos <jezz.santos@hotmail.com>
2022-11-07 08:07:38 +1300

    Removed MVC dependency

--- Availabilities.Api/Startup.cs
+++ Availabilities.Api/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ServiceStack;
@@ -11,8 +10,6 @@ namespace Availabilities
     {
         public new void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false).AddRazorRuntimeCompilation();
-            services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -21,25 +18,10 @@ namespace Availabilities
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
 
             app.UseServiceStack(new ServiceHost
             {
                 AppSettings = new NetCoreAppSettings(Configuration)
-            });
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}");
-
-                // Catches all our SPA routes
-                endpoints.MapFallbackToController("Index", "Home");
             });
         }
     }
